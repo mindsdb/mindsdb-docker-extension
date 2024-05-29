071c619f9102d84d7993ec02fbed7baced3ecf66
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-05-29 17:57:49 +0530

    re-arranged imports to improve readability

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,9 +1,9 @@
-import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import CssBaseline from "@mui/material/CssBaseline";
 import { DockerMuiThemeProvider } from "@docker/docker-mui-theme";
 
+import "./index.css";
 import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
